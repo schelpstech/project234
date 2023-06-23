79c7524efe0ef85b4777ce03658b961fc86638e4
schelpstech <sunkanmi@schelps.com.ng>
2023-06-23 09:12:18 +0100

    update on header

--- pages/school/inc/header.php
+++ pages/school/inc/header.php
@@ -16,37 +16,16 @@
     <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../../assets/img/favicon.png">
     <title>
-        Corporate UI Dashboard PRO by Creative Tim
+        CRSM Portal - School  
     </title>
 
 
     <link rel="canonical" href="https://www.creative-tim.com/product/corporate-ui-dashboard-pro" />
 
     <meta name="keywords"
-        content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap 5 dashboard, bootstrap 5, css3 dashboard, bootstrap 5 admin, Corporate UI bootstrap 5 dashboard, frontend, responsive bootstrap 5 dashboard, soft design, soft dashboard bootstrap 5 dashboard">
+        content="CRSM Portal - School, CRSM, CRSM Schools, RCCG Schools">
     <meta name="description"
-        content="Corporate UI Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
-
-    <meta name="twitter:card" content="product">
-    <meta name="twitter:site" content="@creativetim">
-    <meta name="twitter:title" content="Corporate UI Dashboard PRO by Creative Tim">
-    <meta name="twitter:description"
-        content="Corporate UI Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
-    <meta name="twitter:creator" content="@creativetim">
-    <meta name="twitter:image"
-        content="https://s3.amazonaws.com/creativetim_bucket/products/727/original/corporate-ui-dashboard-pro.jpg?1678117894">
-
-    <meta property="fb:app_id" content="655968634437471">
-    <meta property="og:title" content="Corporate UI Dashboard PRO by Creative Tim" />
-    <meta property="og:type" content="article" />
-    <meta property="og:url"
-        content="https://demos.creative-tim.com/corporate-ui-dashboard-pro/pages/dashboards/default.html" />
-    <meta property="og:image"
-        content="https://s3.amazonaws.com/creativetim_bucket/products/727/original/corporate-ui-dashboard-pro.jpg?1678117894" />
-    <meta property="og:description"
-        content="Corporate UI Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you." />
-    <meta property="og:site_name" content="Creative Tim" />
-
+        content="Christ The Redeemers School Management Online Portal - School Users">
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Noto+Sans:300,400,500,600,700,800|PT+Mono:300,400,500,600,700"
         rel="stylesheet" />
