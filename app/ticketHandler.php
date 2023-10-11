ddb5705e202b76ffbbc9d82bf4c0d55f87c326ab
schelpstech <sunkanmi@schelps.com.ng>
2023-10-11 12:26:53 +0100

    Fixed

--- app/ticketHandler.php
+++ app/ticketHandler.php
@@ -104,16 +104,16 @@ elseif (isset($_POST['replyTicketForm']) && isset($_SESSION['activeAdmin']) ) {
             ];
     
             if ($model->upDate('_tbl_ticket', $ticket_data, $condition) == true && $model->insert_data('_tbl_conversation', $conversation_data) == true) {
-                $user->recordLog( $_POST['sch_code'], 'Support Ticket Reply', 'A new reply has been updated on support ticket #' . $_SESSION['ticketid'] . ' for school with code : ' . $_POST['schCode']);
+                $user->recordLog( $_POST['sch_code'], 'Support Ticket Reply', 'A new reply has been updated on support ticket #' . $_SESSION['ticketid'] . ' for school with code : ' . $_POST['sch_code']);
                 $utility->notifier('success', 'Your reply has been submitted for school with code: ' . $_POST['sch_code']);
-                $model->redirect('./adminRouter.php?pageid=' . base64_encode('conversation') . '&ticketid=' . $_SESSION['ticketid'].'&schCode='.$_POST['schCode']);
+                $model->redirect('./adminRouter.php?pageid=' . base64_encode('conversation') . '&ticketid=' . $_SESSION['ticketid'].'&schCode='.$_POST['sch_code']);
             } else {
                 $utility->notifier('dark', 'There was an error submitting the reply to your support ticket for school with code: ' . $_POST['sch_code']);
-                $model->redirect('./adminRouter.php?pageid=' . base64_encode('conversation') . '&ticketid=' . $_SESSION['ticketid'].'&schCode='.$_POST['schCode']);
+                $model->redirect('./adminRouter.php?pageid=' . base64_encode('conversation') . '&ticketid=' . $_SESSION['ticketid'].'&schCode='.$_POST['sch_code']);
             }
         } else {
             $utility->notifier('danger', 'There are some missing fields. Ensure all fields are inputed.');
-            $model->redirect('./adminRouter.php?pageid=' . base64_encode('conversation') . '&ticketid=' . $_SESSION['ticketid'].'&schCode='.$_POST['schCode']);
+            $model->redirect('./adminRouter.php?pageid=' . base64_encode('conversation') . '&ticketid=' . $_SESSION['ticketid'].'&schCode='.$_POST['sch_code']);
         }
     
 }
